2aa15f779102a295bb343daf048c02be2019e8c9
Jonathan Green <jonathangreen@jonathans-imac.fios-router.home>
2017-09-22 07:46:39 -0700

    Initial Commit

---
+++ Ripple_ExchangeTests/Ripple_ExchangeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ripple_ExchangeTests.swift
+//  Ripple_ExchangeTests
+//
+//  Created by Jonathan Green on 9/22/17.
+//  Copyright © 2017 ripple. All rights reserved.
+//
+
+import XCTest
+@testable import Ripple_Exchange
+
+class Ripple_ExchangeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
